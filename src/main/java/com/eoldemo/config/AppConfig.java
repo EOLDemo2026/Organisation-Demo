eeb100b3be2d80bd65b599f2192ea68ca0597ea5
EOLDemo2026 <girishs204@gmail.com>
2026-08-12 20:02:59 +0530

    Add AppConfig for Spring component scanning

---
+++ src/main/java/com/eoldemo/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.eoldemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.eoldemo")
+public class AppConfig {
+}

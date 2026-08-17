b8195a0614cc2b8c0e09d74239a2577e40dcac79
EOLDemo2026 <girishs204@gmail.com>
2026-08-17 15:59:51 +0530

    Update GreetingServiceImpl to use ConfigUtil and commons-lang3

--- src/main/java/com/eoldemo/service/GreetingServiceImpl.java
+++ src/main/java/com/eoldemo/service/GreetingServiceImpl.java
@@ -1,11 +1,22 @@
 package com.eoldemo.service;
 
+import com.eoldemo.config.ConfigUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GreetingServiceImpl implements GreetingService {
+
+    private final ConfigUtil configUtil;
+
+    public GreetingServiceImpl(ConfigUtil configUtil) {
+        this.configUtil = configUtil;
+    }
+
     @Override
     public String greet(String name) {
-        return "Hello, " + (name == null ? "World" : name) + "!";
+        String prefix = configUtil.getGreetingPrefix();
+        String who = StringUtils.isBlank(name) ? "World" : name;
+        return prefix + who + "!";
     }
 }

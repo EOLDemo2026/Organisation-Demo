b7cba16d8b48d3bb837a4ad4ff7194a589d2c92a
EOLDemo2026 <girishs204@gmail.com>
2026-08-17 16:03:04 +0530

    Update MainApp to perform programmatic login via AuthService before greeting

--- src/main/java/com/eoldemo/MainApp.java
+++ src/main/java/com/eoldemo/MainApp.java
@@ -1,15 +1,28 @@
 package com.eoldemo;
 
 import com.eoldemo.config.AppConfig;
+import com.eoldemo.security.AuthService;
 import com.eoldemo.service.GreetingService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AuthService auth = ctx.getBean(AuthService.class);
+        // demo credentials: user / password (in-memory)
+        boolean logged = auth.login("user", "password");
+        if (!logged) {
+            System.err.println("Authentication failed - exiting");
+            ctx.close();
+            return;
+        }
+
         GreetingService svc = ctx.getBean(GreetingService.class);
         String name = (args != null && args.length > 0) ? args[0] : "World";
         System.out.println(svc.greet(name));
+
+        auth.logout();
         ctx.close();
     }
 }
